2db063c6b7ee3b0b11547b71a95530f4303a7dd1
devterse <devterse@gmail.com>
2023-05-22 22:18:02 +0900

    first commit

---
+++ src/main/java/com/devterse/miniware/MiniwareApplication.java
@@ -0,0 +1,13 @@
+package com.devterse.miniware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devterse/miniware/MiniwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devterse.miniware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
